0f2d098a4f989114907756f7094e30fa961b30b4
kc-leung <chiu-leung@hotmail.com>
2023-01-20 14:23:54 -0500

    storybook - add dashboard table with bar columns samples

--- src/util/handlers.js
+++
@@ -1,7 +0,0 @@
-export const sample = Object.entries(_sample).reduce((acc, [key, vals]) => {
-  const currentVals = Object.values(vals)
-  if (acc.length) {
-    return acc.map((a, i) => ({ ...a, [key]: currentVals[i] }))
-  }
-  return currentVals.map((val) => ({ [key]: val }))
-}, [])
