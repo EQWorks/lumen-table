b2b76d12d45342bf7333bad958aa06c25bf87637
kc-leung <chiu-leung@hotmail.com>
2023-02-03 16:07:10 -0500

    tableStyle - add compactTable variant for quick prop access

--- src/tableStyle.js
+++ src/tableStyle.js
@@ -1,108 +1,108 @@
 import { makeStyles } from '@eqworks/lumen-labs'
 
-const tableStyle = ({ headerTitle, centerHeader }) => makeStyles(({
+const tableStyle = ({ headerTitle, centerHeader, compactTable }) => makeStyles(({
   tableRootContainer: {
-    '& .table__container': {
-      margin: headerTitle ? '0.625rem 0.625rem' : 'initial',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
 
-      '& .table__content-container': {
-        position: 'relative',
-        width: '100%',
-        height: 'auto',
-        display: 'inline-block',
-        fontSize: '0.857rem',
-        lineHeight: '1rem',
-        letterSpacing: '0.025rem',
+    '& .table__content-container': {
+      position: 'relative',
+      height: 'auto',
+      margin: headerTitle ? '0.625rem 0.625rem' : 'initial',
+      display: 'inline-block',
+      fontSize: '0.857rem',
+      lineHeight: '1rem',
+      letterSpacing: '0.025rem',
+      borderStyle: 'solid',
+      borderWidth: '1px',
+      borderRadius: '4px',
+      borderCollapse: 'separate',
+      borderSpacing: 0,
+      overflowY: 'auto',
+
+      '& tr': {
         borderStyle: 'solid',
-        borderWidth: '1px',
-        borderRadius: '4px',
         borderCollapse: 'separate',
         borderSpacing: 0,
-        overflowY: 'auto',
 
-        '& tr': {
+        '& td, th': {
+          padding: compactTable ? '0.25rem 0 0.25rem 0.5rem' : '0.75rem 0 0.75rem 1rem',
           borderStyle: 'solid',
           borderCollapse: 'separate',
           borderSpacing: 0,
 
-          '& td, th': {
-            padding: '0.75rem 0 0.75rem 1rem',
-            borderStyle: 'solid',
-            borderCollapse: 'separate',
-            borderSpacing: 0,
-
-            '&:last-child': {
-              paddingRight: '1rem',
-            },
-          },
-
-          '& .border-horizontal': {
-            borderWidth: '0 0 1px 0',
-          },
-
-          '& .border-vertical': {
-            borderWidth: '0 1px 0 0',
-          },
-  
-          '& .border-vertical:last-child': {
-            borderRight: '0',
-          },
-
-          '& .border-around': {
-            borderWidth: '0 1px 1px 0',
+          '&:last-child': {
+            paddingRight: compactTable ? '0.5rem' : '1rem',
           },
         },
 
-        '& .table__header': {
+        '& .border-horizontal': {
+          borderWidth: '0 0 1px 0',
+        },
+
+        '& .border-vertical': {
+          borderWidth: '0 1px 0 0',
+        },
+
+        '& .border-vertical:last-child': {
+          borderRight: '0',
+        },
+
+        '& .border-around': {
+          borderWidth: '0 1px 1px 0',
+        },
+      },
+
+      '& .table__header': {
+        width: '100%',
+        display: 'table',
+        tableLayout: 'fixed',
+        fontWeight: 700,
+
+        '& .table__header-row': {
+          '& .table__header-cell': {
+            '& .table__header-item': {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: centerHeader ? 'center' : 'flex-start',
+              textTransform: 'capitalize',
+            },
+          },
+        },
+      },
+
+      '& .sticky-header': {
+        position: 'sticky',
+        top: 0,
+      },
+
+      '& .table__body': {
+        '& .table__body-row': {
           width: '100%',
           display: 'table',
           tableLayout: 'fixed',
-          fontWeight: 700,
-
-          '& .table__header-row': {
-            '& .table__header-cell': {
-              '& .table__header-item': {
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: centerHeader ? 'center' : 'flex-start',
-                textTransform: 'capitalize',
-              },
+          '& .table__body-cell': {
+            '& .table__body-item': {
+              wordWrap: 'break-word',
             },
           },
         },
+      },
 
-        '& .sticky-header': {
-          position: 'sticky',
-          top: 0,
-        },
+      '& .table__footer': {
+        '& .table__footer-row': {
+          borderWidth: '1px 0 0 0',
+          borderStyle: 'solid',
+          borderCollapse: 'separate',
+          borderSpacing: 0,
 
-        '& .table__body': {
-          '& .table__body-row': {
-            width: '100%',
-            display: 'table',
-            tableLayout: 'fixed',
-            '& .table__body-cell': {
-              '& .table__body-item': {
-                wordWrap: 'break-word',
-              },
-            },
-          },
-        },
-
-        '& .table__footer': {
-          '& .table__footer-row': {
+          '& .border-none': {
             borderWidth: '1px 0 0 0',
-            borderStyle: 'solid',
-            borderCollapse: 'separate',
-            borderSpacing: 0,
+          },
 
-            '& .border-none': {
-              borderWidth: '1px 0 0 0',
-            },
-
-            '& .border-vertical': {
-              borderWidth: '1px 0 0 0',
-            },
+          '& .border-vertical': {
+            borderWidth: '1px 0 0 0',
           },
         },
       },
