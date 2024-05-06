5f47f78c7a9f50562d95ee3dd774f4bb4db4d09e
Hamaad Chughtai <Hamaad102@gmail.com>
2024-05-06 09:45:25 -0400

    barColumnsColor - modified value to potentially contain array of two colors

--- src/index.js
+++ src/index.js
@@ -450,7 +450,10 @@ Table.propTypes = {
     PropTypes.array,
   ]),
   formatData: PropTypes.object,
-  barColumnsColor: PropTypes.string,
+  barColumnsColor: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array,
+  ]),
   headerTitle: PropTypes.bool,
   title: PropTypes.string,
   hideRowsPerPage: PropTypes.bool,
