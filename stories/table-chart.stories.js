0b7b32ea970627402a5aa6dd9da3fd5478458581
Pei-Yi Lin <amy83762100@gmail.com>
2024-06-01 00:36:17 -0400

    feat - Implement tooltip to better handle long text in table

--- stories/table-chart.stories.js
+++ stories/table-chart.stories.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Table } from '../src'
 import ageRange from './data/age-range'
 import provinces from './data/provinces'
+import crossCategoryData from './data/cross-category-data'
 import { data, largeData } from './data/dashboard-data'
 import { makeStyles } from '@eqworks/lumen-labs'
 
@@ -268,3 +269,143 @@ export const ageRangeData = () => (
     />
   </div>
 )
+
+export const crossCategoryIndexes = () => {
+  const sumPercentageFormatting = (data, row, key) => {
+    const total = data.reduce((sum, item) => sum + Number(item[key]), 0)
+    const percentage = (row / total) * 100
+    return `${Number(percentage).toFixed(2)}%`
+  }
+
+  const customTableClasses = makeStyles({
+    root: {
+      width: '100%',
+      '.bar-table-root-container': {
+        overflowY: 'scroll',
+        maxHeight: '24.5rem',
+        scrollbarWidth: 'thin',
+        height: 'auto',
+        fontFamily: 'inherit',
+        '.bar-table-content-container': {
+          fontFamily: 'inherit',
+          tableLayout: 'fixed',
+          '.bar-table-header-container': {
+            backgroundColor: 'white',
+            fontFamily: 'inherit',
+            '.bar-table-header-cell': {
+              fontFamily: 'inherit',
+              '.bar-table-header-item': {
+                color: 'black',
+                justifyContent: 'center',
+                fontFamily: 'inherit',
+                textTransform: 'none',
+              },
+            },
+          },
+          '.bar-table-body-cell': {
+            fontFamily: 'inherit',
+            padding: '0.07rem 1rem',
+            '.bar-table-body-item': {
+              fontFamily: 'inherit',
+              color: 'black',
+              fontWeight: 'lighter',
+
+              height: '1.32rem',
+              overflow: 'hidden',
+              '> div': {
+                '> div': {
+                  height: '1.3rem',
+                },
+                '> p': {
+                  fontFamily: 'inherit',
+                  color:'black',
+                },
+              },
+            },
+          },
+        },
+      },
+    } })
+
+  const content = {
+    columns: [
+      { 
+        Header: 'Category',
+        accessor: 'category_name',
+      },
+      { 
+        Header: 'Dollar Spend',
+        accessor: 'total_spend',
+      },
+      {
+        Header: 'Avg Dollar Spend per Customer',
+        accessor: 'avg_spend_per_customer',
+      },
+      {
+        Header: 'Index of Average Spend',
+        accessor: 'avg_spend_per_customer_index',
+      },
+      {
+        Header: 'Avg Transaction Size',
+        accessor: 'avg_transaction_size',
+      },
+      {
+        Header: 'Index of Average Transaction Size',
+        accessor: 'avg_transaction_size_index',
+      },
+      {
+        Header: 'Penetration Rate',
+        accessor: 'penetration_rate',
+      },
+    ],
+    sortBy: [{ id: 'total_spend', desc: true }],
+    barColumns: ['total_spend', 'avg_spend_per_customer', 'avg_spend_per_customer_index', 'avg_transaction_size', 'avg_transaction_size_index','penetration_rate'],
+    formatData: (data) => ({
+      'Dollar Spend': {
+        func: (row) => sumPercentageFormatting(data, row, 'total_spend'),
+        type: '%',
+      },
+      'Avg Dollar Spend per Customer': (val) => `$${Number(val).toFixed(2)}`,
+      'Index of Average Spend': (val) => Number(val).toFixed(2),
+      'Avg Transaction Size': (val) => `$${Number(val).toFixed(2)}`,
+      'Index of Average Transaction Size': (val) => Number(val).toFixed(2),
+      'Penetration Rate': {
+        func: (row) => sumPercentageFormatting(data, row, 'penetration_rate'),
+        type: '%',
+      },
+    }),
+    tooltipColumns: ['category_name'],
+    tooltipConfig: {
+      placement: 'top-start',
+    },
+  }
+
+  return (
+    <div className={noPaginationStyleClasses.rootContainer}>
+      <div className={customTableClasses.root}>
+        <Table
+          classes={{
+            tableRootContainer: 'bar-table-root-container',
+            tableContentContainer: 'bar-table-content-container',
+            tableHeaderContainer: 'bar-table-header-container',
+            tableHeaderCell: 'bar-table-header-cell',
+            tableHeaderItem: 'bar-table-header-item',
+            tableBodyCell: 'bar-table-body-cell',
+            tableBodyItem: 'bar-table-body-item',
+          }} 
+          data={crossCategoryData}
+          columns={content.columns}
+          barColumns={content.barColumns}
+          hidePagination
+          formatData={content.formatData? content.formatData(crossCategoryData) : {}}
+          sortBy={content.sortBy || {}}
+          toolbar={false}
+          barColumnsColor={['#0066a4', '#b7b7b7']}
+          defaultStyles={{ borderType: 'vertical' }}
+          stickyHeader={true}
+          tooltipColumns={content.tooltipColumns}
+          tooltipConfig={content.tooltipConfig}
+        />
+      </div>
+    </div>
+  )}
