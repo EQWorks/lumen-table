2ab8a444b60187167f1e5f791412969bcd00c9d5
Kris Cowie <kriscowie@gmail.com>
2024-09-18 11:17:20 -0400

    bar-table - lint

--- src/in-cell-bar.js
+++ src/in-cell-bar.js
@@ -100,7 +100,7 @@ const InCellBar = ({ data, column, value, barColumns, formatData, barColumnsColo
   }
   const _maxVals = useMemo(() => computeMaxVals(data, column.id, maxValsPerColumn), [data, column, maxValsPerColumn])
 
-  const numDigits = _value.replace(/[^0-9]/g, '').length;
+  const numDigits = _value.replace(/[^0-9]/g, '').length
 
   const scalingFactor = Math.min(90 + (numDigits * 3), 100)
 
